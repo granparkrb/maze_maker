71cf3ed369e55992bbe9d4d05a5a4982a5839bd3
rightgo09 <skyarrow09@gmail.com>
2013-03-01 20:31:47 +0900

    work in progress

--- r9/lib/maze_maker.rb
+++ r9/lib/maze_maker.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class Maze
   def initialize(data)
     @workers = Array.new
@@ -15,7 +17,7 @@ class Maze
   end
 
   def run
-    @workers.each{ |worker| worker.dig }
+    @workers.each{ |worker| worker.dig(self) }
   end
 
   class Cell
@@ -33,13 +35,34 @@ class Maze
   end
 
   class Worker
-    def initialize(i,j)
-      @i = i
-      @j = j
+    def initialize(row, col)
+      @row = row
+      @col = col
     end
     
-    def dig
+    def dig(maze)
+      pattern = {}
+      pattern["upper-left"]  = [@row - 1, @col - 1] # 左上
+      pattern["upper"]       = [@row - 1, @col    ] # 上
+      pattern["upper-right"] = [@row - 1, @col + 1] # 右上
+      pattern["left"]        = [@row    , @col - 1] # 左
+      pattern["right"]       = [@row    , @col + 1] # 右
+      pattern["lower-left"]  = [@row + 1, @col - 1] # 左下
+      pattern["lower"]       = [@row + 1, @col    ] # 下
+      pattern["lower-right"] = [@row + 1, @col + 1] # 右下
+
       
+
+      # 上のセルは掘れる？
+      row = pattern["upper"][0]
+      col = pattern["upper"][1]
+      maze[row][col].wall?
+
+      pattern.each do |row, col|
+        # 壁だったら次のパターン
+        next if row < 0 || col < 0
+        p row, col
+      end
     end
   end
 end
